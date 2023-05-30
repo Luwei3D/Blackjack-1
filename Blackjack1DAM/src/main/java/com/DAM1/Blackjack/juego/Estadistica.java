024abbcbc44fbe9e7c248f7f213a928e41e3c986
Victor <rusvictor@hotmail.es>
2023-05-30 22:46:47 +0200

    Creando metodos dentro de estadistica para recibir el jugador que ha ganado menos y mas partidos.

--- Blackjack1DAM/src/main/java/com/DAM1/Blackjack/juego/Estadistica.java
+++ Blackjack1DAM/src/main/java/com/DAM1/Blackjack/juego/Estadistica.java
@@ -30,7 +30,52 @@ public class Estadistica {
         if(ganado = true)
             historico.add(new Historico(ganador,participantes));
     }
-
+    protected String JugadorConMasVictorias(){
+        int ganador = 0;
+        int counterOfGanador = Integer.MIN_VALUE;
+        int counter;
+        StringBuilder sb = new StringBuilder();
+        if(historico.size() > 0){
+            for(int i = 0; i < historico.size();i++){
+                counter = 0;
+                for (int j = 0; j < historico.size();j++ ){
+                    if(historico.get(i).getGanador().equalsIgnoreCase(historico.get(j).getGanador())){
+                        counter++;
+                    }
+                }
+                if(counter > counterOfGanador){
+                    ganador = historico.get(i).getId();
+                    counterOfGanador = ganador;
+                }
+            }
+            sb.append(historico.get(ganador).getGanador());
+        }else
+            sb.append("El historico esta vacio");
+        return sb.toString();
+    }
+    protected String JugadorConMenosVictorias(){
+        int ganador = 0;
+        int counterOfGanador = Integer.MAX_VALUE;
+        int counter;
+        StringBuilder sb = new StringBuilder();
+        if(historico.size() > 0){
+            for(int i = 0; i < historico.size();i++){
+                counter = 0;
+                for (int j = 0; j < historico.size();j++ ){
+                    if(historico.get(i).getGanador().equalsIgnoreCase(historico.get(j).getGanador())){
+                        counter++;
+                    }
+                }
+                if(counter < counterOfGanador){
+                    ganador = historico.get(i).getId();
+                    counterOfGanador = counter;
+                }
+            }
+            sb.append(historico.get(ganador).getGanador());
+        }else
+            sb.append("El historico esta vacio");
+        return sb.toString();
+    }
 
     @Override
     public String toString() {

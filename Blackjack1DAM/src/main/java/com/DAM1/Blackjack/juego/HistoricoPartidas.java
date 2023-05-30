53b0512c83d5debe1adb91f5d5f3cfa42d2e4778
Victor <rusvictor@hotmail.es>
2023-05-30 22:32:55 +0200

    Creacion de contador de manos ganados parconatr las victorias del cpu y jugador

--- Blackjack1DAM/src/main/java/com/DAM1/Blackjack/juego/HistoricoPartidas.java
+++ Blackjack1DAM/src/main/java/com/DAM1/Blackjack/juego/HistoricoPartidas.java
@@ -1,5 +1,12 @@
 package com.DAM1.Blackjack.juego;
 
 public class HistoricoPartidas {
-
+    private int numVicJug = 0;
+    private int numVicCpu = 0;
+    public int getNumVicJug() {
+        return numVicJug;
+    }
+    public int getNumVicCPU() {
+        return numVicCpu;
+    }
 }

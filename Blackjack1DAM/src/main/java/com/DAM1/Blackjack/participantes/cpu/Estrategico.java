7ceb29f03bcaf19d75ff1677fbe890f6c8c889e5
LuisCorGon <luis.cortes.a@ies1xabia.org>
2023-06-07 23:35:26 +0200

    Nuevos JavaDocs
    Agregación de los comentarios JavaDoc en las clases restantes de: Bot, Estragecio, Jugador y Participante.

--- Blackjack1DAM/src/main/java/com/DAM1/Blackjack/participantes/cpu/Estrategico.java
+++ Blackjack1DAM/src/main/java/com/DAM1/Blackjack/participantes/cpu/Estrategico.java
@@ -4,5 +4,12 @@ import com.DAM1.Blackjack.cartas.Mazo;
 import com.DAM1.Blackjack.participantes.Participante;
 
 public interface Estrategico {
+
+    /**
+     * Estrategia que implementarán los participantes, cada uno a su manera para ser o mas pasivo o agresivos
+     * @param p participantes sobre los que se actuara
+     * @param m y el mazo de donde tendremos la puntuacion de las cartas
+     * @return devolverá un int depeniendo su respuesta
+     */
     public int estrategia(Participante p, Mazo m);
 }
